86cc7be37a4a83926d7033468764d7ae6c374873
NathanielRand <nathanieljrand@gmail.com>
2023-04-11 21:02:07 -0400

    Update env var related info

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -7,13 +7,17 @@ import (
 	"github.com/NathanielRand/webchest-image-converter-api/internal/routes"
 )
 
+const (
+	port = ":8080"
+)
+
 func StartServer() error {
 	// Get the router from the routes package
 	router := routes.SetupRouter()
 
 	// Create an HTTP server with timeouts
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         port,
 		Handler:      router,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
